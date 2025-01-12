db061a8f1992a226ece99dcead871ac1c4ed4329
tutankhAman <132681496+tutankhAman@users.noreply.github.com>
2025-01-12 21:05:18 +0530

    socials tab completed, added links to social icons

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -36,7 +36,19 @@ const Homepage = () => {
                             </div>
                         </div>
                     </div>
-                    <div className='socials'></div>
+                    <div className='socials'>
+                        <div className='title'>Socials</div>
+                        <div className='social-links'>
+                            <a href="https://github.com/tutankhAman" target="_blank" rel="noopener noreferrer">
+                                <img className='social-logo github' src="/images/logos/github.png" alt="GitHub" />
+                            </a>
+                            <a href="https://www.linkedin.com/in/aman-aziz-23b416261/" target="_blank" rel="noopener noreferrer">
+                                <img className='social-logo linkedin' src="/images/logos/linkedin.png" alt="LinkedIn" />
+                            </a>
+                            <img className='social-logo discord' src="/images/logos/discord.png" alt="Discord" />
+                            <img className='social-logo gmail' src="/images/logos/gmail.png" alt="Gmail" />
+                        </div>
+                    </div>
                 </div>
                 <div className='middle-container'>
                     <div className='intro'></div>
