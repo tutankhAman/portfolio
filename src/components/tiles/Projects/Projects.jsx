6327892f07b93e4a013bc433d0914577528f1e4e
tutankhAman <132681496+tutankhAman@users.noreply.github.com>
2025-01-13 21:31:45 +0530

    projects tab completed

--- src/components/tiles/Projects/Projects.jsx
+++ src/components/tiles/Projects/Projects.jsx
@@ -7,7 +7,12 @@ const Projects = () => {
         <div className='title'>
             My Projects
         </div>
-        <div className='project-info'></div>
+        <div className='projects-info'>
+            <strong>• Delivio:</strong> Reward based food delivery system with AI food recommendation.
+            <br></br>
+            <br></br>
+            <strong>• Clutter-Clearing-System:</strong> A simple script to segregate files and organize your directories.
+        </div>
 
     </div>
   )
