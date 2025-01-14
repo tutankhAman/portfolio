0d7434bcc0b1ee517dd817be247776407ff296cd
tutankhAman <132681496+tutankhAman@users.noreply.github.com>
2025-01-14 21:25:23 +0530

    pokemon adjustment

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,10 +5,10 @@ function Navbar() {
     <nav className="navbar">
       <div className="navbar-content">
         <div className="logo">tutankhAman</div>
-        <div className="nav-links">
+        {/* <div className="nav-links">
           <a className="nav-btn" href="#projects">Projects</a>
           <a className="nav-btn" href="#contact">Contact</a>
-        </div>
+        </div> */}
       </div>
     </nav>
   );
